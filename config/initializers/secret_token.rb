290d977d163cd8857790fb67f53c9811c4f98723
dana <dana@triggit.com>
2013-01-17 20:36:42 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Horsebeer::Application.config.secret_token = '45a7177e5ce123f0ddd3908674bd05a54d90121e7946ec677dcc92fb0cc00b0fa79980da2d2e3c4ffe1e6ff169cf6327d182536260da9ee9e72af97ab48d2b0e'
